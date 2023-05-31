300633c29c26b43bdf179e3b6380f80edb78f78d
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-05-31 21:13:01 +0530

    add login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,44 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+import { LoginForm } from "./login-form";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "User login page.",
+};
+
+export default function LoginPage() {
+  return (
+    <div className="container mx-auto h-full max-w-sm flex flex-col items-center justify-center p-8">
+      <div className="flex w-full flex-col justify-center space-y-6">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Create an account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email below to create your account
+          </p>
+        </div>
+        <LoginForm />
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link
+            href="/terms"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="/privacy"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +19,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
